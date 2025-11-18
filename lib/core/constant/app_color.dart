6aba7a5b4d923b348fb733ee6f305fd5e5461732
Bak Nguen P53 <sangsangden@gmail.com>
2025-11-19 06:45:50 +0700

    first commit

---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color backgroundColor = Color(0xFF0f1729);
+  static const Color onBackgroundColor = Color(0xff131d33);
+
+
+  static const Color activeBeginColor = Color(0xff4977f3);
+  static const Color activeEndColor = Color(0xff883eec);
+  
+  static const Color successColor = Color(0xff19ac4f);
+  static const Color pauseColor = Color(0xff121c34);
+  static const Color errorColor =Color(0xffdf2c2c);
+
+  static const Color mainTextColor = Color(0xfff8fafc);
+  static const Color secondTextColor = Color(0xff94a3b8);
+
+  static const Color activeProgressColor = Color(0xff3c83f6);
+  static const Color inactiveProgressColor = Color(0xff1b315b);
+
+  static const Color downloadSpeedTextColor = Color(0xff3c83f6);
+  static const Color cancelTextColor = Color(0xffef4343);
+
+  static const Color navBarActiveColor = Color(0xff3c83f6);
+  static const Color navBarInActiveColor = Color(0xff94a3b8);
+
+
+}
