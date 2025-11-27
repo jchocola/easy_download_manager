27ca6621652da7d9fc86e440cceb425fbe1dc11a
Bak Nguen <sangsangden@gmail.com>
2025-11-27 17:46:59 +0700

    files page

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.easy_download_manager"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
